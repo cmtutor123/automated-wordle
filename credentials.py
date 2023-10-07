aa7e342c591595e751866bda1821d19a35bbb4e4
Caleb <cmtutor123@gmail.com>
2023-10-06 21:31:24 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 phone_number = '###-###-####'
 provider = 'Provider Name, Ex: "Verizon"'
 sender_email = 'example@email.com'
-sender_access_token = 'access token'
+sender_access_token = 'email access token'
